c8d3376abdd6d850e83161e1efdb85a28ec871ba
chesteric31 <chesteric31@gmail.com>
2016-10-31 15:06:47 +0100

    Use of newScheduledThreadPool.

--- src/main/kotlin/Ksacs.kt
+++ src/main/kotlin/Ksacs.kt
@@ -4,53 +4,48 @@ import java.awt.Robot
 import java.awt.event.InputEvent
 import java.awt.event.MouseAdapter
 import java.awt.event.MouseEvent
+import java.util.*
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import javax.swing.JButton
 import javax.swing.JFrame
-import kotlin.concurrent.thread
+import kotlin.concurrent.timerTask
 
 fun main(args: Array<String>) {
     println("Hello Kotlin")
-    var myThread = Thread()
     var frame = JFrame("KSACS")
     frame.layout = GridBagLayout()
+    var scheduledThreadPool = Executors.newScheduledThreadPool(10)
     var clickButton = JButton("Left Click")
-    val clickAdapter : MouseAdapter = object : MouseAdapter() {
+    val clickAdapter: MouseAdapter = object : MouseAdapter() {
         override fun mouseClicked(e: MouseEvent) {
             super.mouseClicked(e)
             println("Click")
-            if (!myThread.isAlive) {
-                var oldX = MouseInfo.getPointerInfo().location.x
-                var oldY = MouseInfo.getPointerInfo().location.y
-                myThread = thread(true, true, null, "myThread", -1,
-                        {
-                            while (true) {
-                                Thread.sleep(1000)
-                                val newX = MouseInfo.getPointerInfo().location.x
-                                val newY = MouseInfo.getPointerInfo().location.y
-                                println("oldX $oldX - oldY $oldY newX $newX newY $newY")
-                                if (oldX != newX && oldY != newY) {
-                                    var robot = Robot()
-                                    robot.mousePress(InputEvent.BUTTON1_MASK)
-                                    robot.mouseRelease(InputEvent.BUTTON1_MASK)
-                                    oldX = newX
-                                    oldY = oldY
-                                }
-                            }
-                        }
-                )
-            }
+            var oldX = MouseInfo.getPointerInfo().location.x
+            var oldY = MouseInfo.getPointerInfo().location.y
+            scheduledThreadPool.scheduleAtFixedRate(timerTask {
+                val newX = MouseInfo.getPointerInfo().location.x
+                val newY = MouseInfo.getPointerInfo().location.y
+                println("oldX $oldX - oldY $oldY newX $newX newY $newY")
+                if (oldX != newX && oldY != newY) {
+                    var robot = Robot()
+                    robot.mousePress(InputEvent.BUTTON1_MASK)
+                    robot.mouseRelease(InputEvent.BUTTON1_MASK)
+                    oldX = newX
+                    oldY = oldY
+                }
+            }, 0, 1000, TimeUnit.MILLISECONDS)
         }
     }
     clickButton.addMouseListener(clickAdapter)
     frame.add(clickButton)
     var stopButton = JButton("Stop")
-    val stopAdapter : MouseAdapter = object : MouseAdapter() {
+    val stopAdapter: MouseAdapter = object : MouseAdapter() {
         override fun mouseClicked(e: MouseEvent) {
             super.mouseClicked(e)
-            println(myThread.state)
-            if (myThread.isAlive) {
-                println("stop ${myThread.state}")
-                myThread.interrupt()
+            if (!scheduledThreadPool.isTerminated) {
+                scheduledThreadPool.shutdown()
+                scheduledThreadPool = Executors.newScheduledThreadPool(10)
             }
         }
     }
